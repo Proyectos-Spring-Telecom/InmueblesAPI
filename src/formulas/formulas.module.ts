8c626dbe514db37d3d354a9c7f403d207e5c72d4
Luis Neyra <luisnm93@gmail.com>
2026-06-17 13:32:51 -0600

    [DAD] correccion de factores

--- src/formulas/formulas.module.ts
+++ src/formulas/formulas.module.ts
@@ -4,13 +4,14 @@ import { BitacoraModule } from "src/bitacora/bitacora.module";
 import { Formulas } from "src/entities/Formulas";
 import { Factores } from "src/entities/Factores";
 import { FormulaEvaluaciones } from "src/entities/FormulaEvaluaciones";
+import { Inpc } from "src/entities/Inpc";
 import { FormulasController } from "./formulas.controller";
 import { FormulasService } from "./formulas.service";
 import { FormulaEngineService } from "./formula-engine.service";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Formulas, Factores, FormulaEvaluaciones]),
+    TypeOrmModule.forFeature([Formulas, Factores, FormulaEvaluaciones, Inpc]),
     BitacoraModule,
   ],
   controllers: [FormulasController],
